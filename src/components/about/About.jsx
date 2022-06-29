8f8a2f5a47bf58c1d0af72e82f35c378bf0343af
Lucas Linha <lucas.linha@gmail.com>
2022-06-29 18:31:16 -0300

    [06] Finishing About and Creating Experience Container mark and Styles

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.jpg'
 import {FaAward} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
+import {FiUsers} from 'react-icons/fi'
 
 const about = () => {
   return (
@@ -15,7 +17,6 @@ const about = () => {
             <img src={ME} alt="About"></img>
           </div>
         </div>
-      </div>
 
       <div className="about__content">
         <div className="about__cards">
@@ -26,16 +27,23 @@ const about = () => {
           </article>
 
           <article className='about__card'>
-          <FaAward className='about__icon'/>
-            <h5>Experience</h5>
-            <small>3+ Years of Experience</small>
+          <FiUsers className='about__icon'/>
+            <h5>Clients</h5>
+            <small>10+ Clients</small>
           </article>
           
           <article className='about__card'>
-          <FaAward className='about__icon'/>
-            <h5>Experience</h5>
-            <small>3+ Years of Experience</small>
+          <VscFolderLibrary className='about__icon'/>
+            <h5>Projects</h5>
+            <small>15+ Completed projects</small>
           </article>
+        </div>
+        
+        <p>
+        Lorem ipsum dolor sit amet. Sit incidunt nulla eum quia asperiores aut deserunt sunt. A velit perspiciatis qui nisi quis quo nobis numquam in laudantium expedita qui molestiae nemo. Et dolor tempora in assumenda dolorem hic voluptatem ea magnam explicabo sed alias suscipit. Et voluptatem ipsam cum sequi architecto est quibusdam saepe sit necessitatibus internos eum laborum veritatis vel culpa internos ut assumenda sapiente.
+        </p>
+
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
